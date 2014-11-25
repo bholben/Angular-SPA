9ac573dee74625d5a63f26fc90af6eb0718d42e1
Bob Holben <bholben@gmail.com>
2014-11-24 23:45:23 -0500

    Switch to angular-ui-router.

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -2,57 +2,29 @@
   'use strict';
 
   angular
-    .module('routes', ['ngRoute'])
+    .module('routes', ['ui.router'])
     .config(routes);
 
-  function routes($routeProvider) {
-    $routeProvider
-      .when('/', {
+  function routes($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('index', {
+        url: '/',
         templateUrl: '/app/home/home.html'
       })
-      .when('/plane', {
+      .state('plane', {
+        url: '/plane',
         templateUrl: '/app/plane/plane.html'
       })
-      .when('/car', {
+      .state('car', {
+        url: '/car',
         templateUrl: '/app/car/car.html'
       })
-      .when('/bicycle', {
+      .state('bicycle', {
+        url: '/bicycle',
         templateUrl: '/app/bicycle/bicycle.html'
-      })
-      .otherwise({ redirectTo: '/' });
+      });
   }
 
 }());
-
-
-// (function () {
-//   'use strict';
-
-//   angular.module('myApp', ['ui.router']);
-
-//   var routeProvider = function ($stateProvider, $urlRouterProvider) {
-
-//     // For unmatched routes
-//     $urlRouterProvider.otherwise('/');
-
-//     // Application routes
-//     $stateProvider
-//       .state('index', {
-//         url: '/',
-//         templateUrl: '/app/home/home.html'
-//       })
-//       .state('planes', {
-//         url: '/planes',
-//         templateUrl: '/app/planes/planes.html'
-//       })
-//       .state('cars', {
-//         url: '/cars',
-//         templateUrl: '/app/cars/cars.html'
-//       });
-//   };
-
-//   angular.module('myApp').config('routeProvider', routeProvider);
-//   routeProvider.$inject = ['$stateProvider', '$urlRouterProvider'];
-
-// }());
 
